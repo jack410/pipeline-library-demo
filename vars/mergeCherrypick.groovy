7cd94c7e74b54355ec044365070e09b5bd071d5d
junjun <jack410@163.com>
2018-03-07 18:59:00 +0800

    fix error

--- vars/mergeCherrypick.groovy
+++ vars/mergeCherrypick.groovy
@@ -1,5 +1,5 @@
 def call(String name = 'test') {
-    switch(name) {
+    switch(validation) {
                 case "test": 
                 git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
                     break; 
