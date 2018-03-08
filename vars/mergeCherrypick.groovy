38a3f51c532aa0608c5e08ed8184e35edc3a2612
junjun <jack410@163.com>
2018-03-08 17:13:40 +0800

    fix scripts

--- vars/mergeCherrypick.groovy
+++ vars/mergeCherrypick.groovy
@@ -1,29 +1,29 @@
-def call(String name) {
+def call(String name, String module, String merge, String cid) {
     switch(name) {
                 case "test": 
-                git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
+                git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
                     break; 
                 case "validation": 
                 echo "Hello, ${name}."
-                git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
-                git branch: "validation", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
+                git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
+                git branch: "validation", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
                 sh 'git branch'
-                if (CID) {
+                if (cid) {
                     sh '''
                     #!/bin/bash
-                    echo $CID
-                    arr=$(echo $CID|tr "," "\n")
+                    echo $cid
+                    arr=$(echo $cid|tr "," "\n")
                     for x in $arr; do
                     git cherry-pick --strategy=recursive -X theirs $x 
                     done
                     git push
                     '''
-                    } else if (MERGE == "yes") {
+                    } else if (merge == "yes") {
                         sh '''
                         git merge -X theirs master
                         git push
                         '''
-                    } else if (MERGE == 'no') {
+                    } else if (merge == 'no') {
                         println("No commit message.");
                         break;
                     }
@@ -32,22 +32,22 @@ def call(String name) {
                 git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
                 git branch: "validation", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
                 git branch: "release", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
-                if (CID) {
+                if (cid) {
                     sh '''
                     #!/bin/bash
-                    echo $CID
-                    arr=$(echo $CID|tr "," "\n")
+                    echo $cid
+                    arr=$(echo $cid|tr "," "\n")
                     for x in $arr; do
                     git cherry-pick --strategy=recursive -X theirs $x 
                     done
                     git push
                     '''
-                    } else if (MERGE == "yes") {
+                    } else if (merge == "yes") {
                         sh '''
                         git merge -X theirs validation
                         git push
                         '''
-                    } else (MERGE == 'no') {
+                    } else (merge == 'no') {
                         println("No commit message.");
                     }
                     break; 
