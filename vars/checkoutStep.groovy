8c2542259977141311a6a9c5b3ec465bf0967b6e
junjun <jack410@163.com>
2018-03-29 17:32:37 +0800

    add output

--- vars/checkoutStep.groovy
+++ vars/checkoutStep.groovy
@@ -11,7 +11,7 @@ def call(String env, String module, version = '') {
  	def env2branch = [test: 'master', validation: 'validation', prod: 'release']
 
  	def branch = env2branch[env] ?: 'master'
-
+ 	echo "env is ${env}."
 	git branch: "${branch}", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
 	commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
     commitId = commitId.trim()
