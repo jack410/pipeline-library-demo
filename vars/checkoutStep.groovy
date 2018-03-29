0d139e0a01b4bb280a944cd32a3f279219bf0ae8
junjun <jack410@163.com>
2018-03-29 16:33:48 +0800

    fix error

--- vars/checkoutStep.groovy
+++ vars/checkoutStep.groovy
@@ -13,19 +13,19 @@ def call(String env, String module) {
     	git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
         commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
         commitId = commitId.trim()
-        IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+        // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
         break;
         case "validation":
         git branch: "validation", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
         commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
         commitId = commitId.trim()
-        IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+        // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
         break;
         case "prod":
         git branch: "release", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
         commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
         commitId = commitId.trim()
-        IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+        // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
         break;
     }
 }
