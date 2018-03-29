47ce471285308eb09ed20a9f295e9e2c5c294bb5
junjun <jack410@163.com>
2018-03-29 15:58:24 +0800

    add checkoutStep.groovy

---
+++ vars/checkoutStep.groovy
@@ -0,0 +1,31 @@
+def call(String env, String module, String version) {
+	if (ENV == 'test') {
+            BRANCH = 'master'
+        } 
+        else if (ENV == 'validation') {
+            BRANCH = 'validation'
+        }
+        else if (ENV == 'prod') {
+            BRANCH = 'release'
+        }
+    switch(env) {
+    	case "test":
+    	git branch: "${BRANCH}", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
+        commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
+        commitId = commitId.trim()
+        IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+        break;
+        case "validation":
+        git branch: "${BRANCH}", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
+        commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
+        commitId = commitId.trim()
+        IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+        break;
+        case "prod":
+        git branch: "${BRANCH}", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
+        commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
+        commitId = commitId.trim()
+        IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${version}"
+        break;
+    }
+}
