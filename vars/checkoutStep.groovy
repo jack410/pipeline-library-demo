490bb42d84ee42b8beb1c810c8c9d59f822e76ee
junjun <jack410@163.com>
2018-03-29 17:28:39 +0800

    fix script

--- vars/checkoutStep.groovy
+++ vars/checkoutStep.groovy
@@ -1,4 +1,4 @@
-def call(String env, String module) {
+def call(String env, String module, version = '') {
 	// if (env == 'test') {
  //            BRANCH = 'master'
  //        } 
@@ -8,26 +8,36 @@ def call(String env, String module) {
  //        else if (env == 'prod') {
  //            BRANCH = 'release'
  //        }
-    switch(env) {
-    	case "test":
-    	echo "env is ${env}."
-        echo "module is ${module}"
-    	git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
-     //    commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
-     //    commitId = commitId.trim()
-        // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
-        break;
-        case "validation":
-        git branch: "validation", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
-        commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
-        commitId = commitId.trim()
-        // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
-        break;
-        case "prod":
-        git branch: "release", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
-        commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
-        commitId = commitId.trim()
-        // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
-        break;
+ 	def env2branch = [test: 'master', validation: 'validation', prod: 'release']
+
+ 	def branch = env2branch[env] ?: 'master'
+
+	git branch: "${branch}", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
+	commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
+    commitId = commitId.trim()
+    IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+
+
+    // switch(env) {
+    // 	case "test":
+    // 	echo "env is ${env}."
+    //     echo "module is ${module}"
+    // 	git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
+    //  //    commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
+    //  //    commitId = commitId.trim()
+    //     // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+    //     break;
+    //     case "validation":
+    //     git branch: "validation", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
+    //     commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
+    //     commitId = commitId.trim()
+    //     // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+    //     break;
+    //     case "prod":
+    //     git branch: "release", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
+    //     commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
+    //     commitId = commitId.trim()
+    //     // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+    //     break;
     }
 }
