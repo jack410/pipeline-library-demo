e1dd369568500f6a49f1f5fdadfa1fb5867232f5
junjun <jack410@163.com>
2018-03-30 10:27:35 +0800

    fix script

--- vars/checkoutStep.groovy
+++ vars/checkoutStep.groovy
@@ -1,43 +1,23 @@
 def call(String env, String module, version = '') {
-	// if (env == 'test') {
- //            BRANCH = 'master'
- //        } 
- //        else if (env == 'validation') {
- //            BRANCH = 'validation'
- //        }
- //        else if (env == 'prod') {
- //            BRANCH = 'release'
- //        }
- 	def env2branch = [test: 'master', validation: 'validation', prod: 'release']
 
- 	def branch = env2branch[env] ?: 'master'
- 	echo "env is ${env}."
-	git branch: "${branch}", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
-	commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
-    commitId = commitId.trim()
-    return commitId
-    // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+    stage ('checkout'){
+        def env2branch = [test: 'master', validation: 'validation', prod: 'release']
 
-    // switch(env) {
-    // 	case "test":
-    // 	echo "env is ${env}."
-    //     echo "module is ${module}"
-    // 	git branch: "master", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
-    //  //    commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
-    //  //    commitId = commitId.trim()
-    //     // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
-    //     break;
-    //     case "validation":
-    //     git branch: "validation", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
-    //     commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
-    //     commitId = commitId.trim()
-    //     // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
-    //     break;
-    //     case "prod":
-    //     git branch: "release", credentialsId: 'xsio', url: "git@github.com:xsio/${MODULE}.git"
-    //     commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
-    //     commitId = commitId.trim()
-    //     // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
-    //     break;
-    // }
+        def branch = env2branch[ENV] ?: 'master'
+        git branch: "${branch}", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
+        commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
+        commitId = commitId.trim()
+        echo commitId
+        // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
+    }
+    stage ('build'){
+
+       sh '''
+            #bypass jenkins $HOME bug
+            export HOME=/opt/hudson
+            . ${HOME}/.bashrc
+            ./gradlew clean
+            ./gradlew bootRepackage -Dgrails.env=${ENV}
+        '''
+    }
 }
