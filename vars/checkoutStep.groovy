0298ac0650b13afc97b8308b54ff84c469c78931
junjun <jack410@163.com>
2018-03-30 10:33:15 +0800

    fix script

--- vars/checkoutStep.groovy
+++ vars/checkoutStep.groovy
@@ -1,8 +1,8 @@
 def call(String env, String module, version = '') {
-	
+
         def env2branch = [test: 'master', validation: 'validation', prod: 'release']
 
-        def branch = env2branch[ENV] ?: 'master'
+        def branch = env2branch[env] ?: 'master'
         git branch: "${branch}", credentialsId: 'xsio', url: "git@github.com:xsio/${module}.git"
         commitId = sh returnStdout: true, script: 'git rev-parse HEAD'
         commitId = commitId.trim()
