7accb6d8164b49d950e5dd116d44a7660016ed47
junjun <jack410@163.com>
2018-03-30 10:31:39 +0800

    fix script

--- vars/checkoutStep.groovy
+++ vars/checkoutStep.groovy
@@ -1,6 +1,5 @@
 def call(String env, String module, version = '') {
-
-    stage ('checkout'){
+	
         def env2branch = [test: 'master', validation: 'validation', prod: 'release']
 
         def branch = env2branch[ENV] ?: 'master'
@@ -9,15 +8,4 @@ def call(String env, String module, version = '') {
         commitId = commitId.trim()
         echo commitId
         // IMAGE = "${env.REGISTRY_SNAPSHOT}/${IMAGE_PATH}:${commitId}"
-    }
-    stage ('build'){
-
-       sh '''
-            #bypass jenkins $HOME bug
-            export HOME=/opt/hudson
-            . ${HOME}/.bashrc
-            ./gradlew clean
-            ./gradlew bootRepackage -Dgrails.env=${ENV}
-        '''
-    }
 }
